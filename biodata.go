8f501c655788bd35510d97fc5bb553ffa88c6126
JaringanKantor.com <nc.anwar@jaringankantor.com>
2021-10-15 22:06:00 +0700

    Update biodata.go

--- biodata.go
+++ biodata.go
@@ -4,6 +4,12 @@ import "fmt"
 
 type Active func(string) bool
 
+type Pegawai struct {
+	LamaBekerja  int8
+	Nama, Divisi string
+	Aktif        bool
+}
+
 func User(name string, isAktif Active) {
 
 	if isAktif(name) {
@@ -11,4 +17,15 @@ func User(name string, isAktif Active) {
 	} else {
 		fmt.Println(name, " Status Non-AKtif")
 	}
+}
+
+func TampilData() {
+	pegawai := Pegawai{
+		LamaBekerja: 5,
+		Nama:        "Anwar",
+		Divisi:      "Keuangan",
+		Aktif:       true,
+	}
+
+	fmt.Println(pegawai)
 }
