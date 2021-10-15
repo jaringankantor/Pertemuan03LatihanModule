96924505f6369282f7fadde4e0d36ec80bf0dd49
JaringanKantor.com <nc.anwar@jaringankantor.com>
2021-10-15 19:38:17 +0700

    anonymous function

--- biodata.go
+++ biodata.go
@@ -1,2 +1,14 @@
 package module
 
+import "fmt"
+
+type Active func(string) bool
+
+func User(name string, isAktif Active) {
+
+	if isAktif(name) {
+		fmt.Println(name, " Status Aktif")
+	} else {
+		fmt.Println(name, " Status Non-AKtif")
+	}
+}
